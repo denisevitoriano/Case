ef598b873c1e67fc64d6a87053a6ca803e05c453
Denise Vitoriano <denisevitoriano@gmail.com>
2021-03-12 20:41:42 -0300

    instruções para criar um projeto

---
+++ 00_instrucoes.R
@@ -0,0 +1,7 @@
+# Criando o projeto (pasta)
+usethis::create_project("C:/Users/denis/Google Drive/Cursos/DSAV_ASN_ROCKS/Case")
+
+# Iniciando o git: seleciona opcao sim em tudo
+usethis::use_git()
+
+
